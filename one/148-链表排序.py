3bd70db48504acd6a9966a7cba4c6f19959b6eec
dragon <dragonax@163.com>
2024-01-30 14:14:55 +0800

    148. 排序链表

--- one/148-链表排序.py
+++ one/148-链表排序.py
@@ -7,6 +7,10 @@
 难度：中等
 
 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表。
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/sort-list/
 """
 
 
@@ -42,28 +46,46 @@ def newList(ls: list):
 
 
 class Solution:
-    @classmethod
-    def addTwoNumbers(cls, l1: ListNode, l2: ListNode) -> ListNode:
-        p = ListNode(0)
-        q = p  # p的第一个节点0在最终返回时被去掉了。q 是指向 p中第一个节点的指针。
-        tmp = 0
+    def sortList(self, head: ListNode) -> ListNode:
+        def sortFunc(head: ListNode, tail: ListNode) -> ListNode:
+            if not head:
+                return head
+            if head.next == tail:
+                head.next = None
+                return head
+            slow = fast = head
+            while fast != tail:
+                slow = slow.next
+                fast = fast.next
+                if fast != tail:
+                    fast = fast.next
+            mid = slow
+            return merge(sortFunc(head, mid), sortFunc(mid, tail))
 
-        while l1 or l2 or tmp:
-            if l1:
-                tmp += l1.val
-                l1 = l1.next
-            if l2:
-                tmp += l2.val
-                l2 = l2.next
+        def merge(head1: ListNode, head2: ListNode) -> ListNode:
+            dummyHead = ListNode(0)
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
 
-            p.next = ListNode(tmp % 10)
-
-            p = p.next  # p在循环中不断地重新指向结果数字的更高位节点
-            tmp = tmp // 10
-
-        return q.next
+        return sortFunc(head, None)
 
 
 if __name__ == '__main__':
     list_node1 = newList([4, 2, 1, 3])
     print(f"list_node1 = {list_node1}")
+
+    s = Solution()
+    res = s.sortList(list_node1)
+    print(f"res = {res}")
